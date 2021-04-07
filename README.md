abe2187bc0b272a4599762c7f08b887033babf8e
wangqs <wqsacy@vip.qq.com>
2021-04-07 13:41:01 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center"> bullshit </h1>
+
+<p align="center"> 狗屁不通文章生成器PHP版.</p>
+
+
+## Installing
+
+```shell
+$ composer require wangqs/bullshit -vvv
+```
+
+## Usage
+
+```
+use Wangqs\Bullshit;
+$bullshit = new Bullshit();
+echo $bullshit->generator("hello word",1000);
+```
+
+## Contributing
+
+You can contribute in one of three ways:
+
+1. File bug reports using the [issue tracker](https://github.com/wangqs/bullshit/issues).
+2. Answer questions or fix bugs on the [issue tracker](https://github.com/wangqs/bullshit/issues).
+3. Contribute new features or update the wiki.
+
+_The code contribution process is not very formal. You just need to make sure that you follow the PSR-0, PSR-1, and PSR-2 coding guidelines. Any new code contributions must be accompanied by unit tests where applicable._
+
+## License
+
+MIT
+
+## 参考python版本
+menzi11/BullshitGenerator
---
+++ src/Bullshit.php
@@ -0,0 +1,45 @@
+<?php
+
+	namespace Wangqs\Bullshit;
+
+	/**
+	 *
+	 * Created by Malcolm.
+	 * Date: 2021/4/7  13:34
+	 */
+	class Bullshit
+	{
+
+		/**
+		 * @param string $title  标题
+		 * @param int    $length 长度
+		 * @param string $data   语料位置
+		 * @return string
+		 * @author     :  Wangqs  2021/4/7
+		 * @description:  主要生成方法
+		 */
+		public function generator ( $title = 'hello world' , $length = 1000 , $data = 'data.json' ) {
+			$body = "";
+			$data = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . $data;
+			$data = json_decode( file_get_contents( $data ) , true );
+			while ( strlen( $body ) / 3 < $length ) {
+
+				$num = rand( 0 , 100 );
+				if ( $num < 10 ) {
+					$body .= "\r\n";
+				}
+				else if ( $num < 20 ) {
+					$body .= $data["famous"][array_rand( $data["famous"] , 1 )];
+					$replace = [ $data["before"][array_rand( $data["before"] , 1 )] , $data['after'][array_rand( $data['after'] , 1 )] ];
+					$find = [ 'a' , 'b' ];
+					$body = str_replace( $find , $replace , $body );
+				}
+				else {
+					$body .= $data["bosh"][array_rand( $data["bosh"] , 1 )];
+				}
+				$body = str_replace( "x" , $title , $body );
+			}
+			return $body;
+		}
+
+	}
